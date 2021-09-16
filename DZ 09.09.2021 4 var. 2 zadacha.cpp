ab777893f146ad6f6222694ce0919f5548cf206e
Vitaliy <61117565+poseidonnn0@users.noreply.github.com>
2021-09-16 08:34:49 +0200

    Add files via upload

---
+++ DZ 09.09.2021 4 var. 2 zadacha.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+
+{
+    setlocale(LC_ALL, "Rus");
+    int N;
+    cout << "Лабораторная работа от 09.09.2021, Кондратьев Виталий, 4 вариант, Задача №2" << '\n';
+    cout << "Введите число N = ";
+    cin >> N;
+    switch (N)
+    {
+        case 0: cout << "Всё хорошо";
+            break;
+        case 1: cout << "Ошибка чтения файла" << '\n';
+            break;
+        case 2: cout << "Ошибка записи файла" << '\n';
+            break;
+        case 3: cout << "Не все поля определены" << '\n';
+            break;
+        default:
+            cout << "Такой цифры/числа нет." << '\n';
+    }
+}
