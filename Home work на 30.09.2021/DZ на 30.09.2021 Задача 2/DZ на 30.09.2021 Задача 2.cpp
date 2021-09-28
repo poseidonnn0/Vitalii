bdbd47de8d587e74c10a401e32348ae7830781bc
Vitaliy <61117565+poseidonnn0@users.noreply.github.com>
2021-09-28 17:36:49 +0200

    Update DZ на 30.09.2021 Задача 2.cpp

--- Home work на 30.09.2021/DZ на 30.09.2021 Задача 2/DZ на 30.09.2021 Задача 2.cpp
+++ Home work на 30.09.2021/DZ на 30.09.2021 Задача 2/DZ на 30.09.2021 Задача 2.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 
 #define N 1000
@@ -14,24 +14,30 @@ using std::cin;
 (3 часть)числа с одинаковыми первыми цифрами и  одинаковыми наименьшими цифрами дополнительно упорядочить по неубыванию самого числа.
 */
 
-int FirstDig(int n) // решение через функцию, чтобы не загромаждать цикл
+
+int FirstDig(int n) // Поиск первой цифры в числе
 {
-	while (n > 9)
+	while ((n/10) > 0)
 	{
 		n /= 10;
 	}
 	return n;
 }
 
-int SumDigits(int n) // Сумма цифр числа
+int MinDigit(int n) // Поиск минимальной цифры в числе
 {
-	int sum = 0;
-	while (n > 9)
-	{
-		sum += (n / 10);
-		n /= 10;
-	}
-	return sum;
+	int Min = 10;
+	int Ostat;
+		while ((n/10) > 0)
+		{
+			Ostat = n % 10;
+			if (Min > Ostat)
+			{
+				Min = Ostat;
+			}
+			n /= 10;
+		}
+		return Min;
 }
 
 int main()
@@ -56,31 +62,45 @@ int main()
 
 		out << endl << endl;
 
-		for (int i = 0; i < n - 1; i++) // sort
+		for (int i = 0; i < n - 1; i++) // sort1
 		{
 			for (int j = i + 1; j < n; j++)
 			{
-				if (FirstDig(mas[i]) != FirstDig(mas[j]))
+				if (FirstDig(mas[i]) > FirstDig(mas[j]))
 				{
-					if (FirstDig(mas[i]) > FirstDig(mas[j]))
+					std::swap(mas[i], mas[j]);
+				}
+			}
+		}
+
+		for (int i = 0; i < n - 1; i++) //sort2
+			{
+				for (int j = i + 1; j < n; j++)
+				{
+					if (FirstDig(mas[i]) == FirstDig(mas[j]))
 					{
-						std::swap(mas[i],mas[j]);
-					}
-				else if (SumDigits(mas[i]) != SumDigits(mas[j]))
-					{
-						if (SumDigits(mas[i]) > SumDigits(mas[j]))
+						if (MinDigit(mas[i]) > MinDigit(mas[j]))
 						{
 							std::swap(mas[i], mas[j]);
 						}
 					}
-				else if (mas[i] > mas[j])
+				}
+			}
+		
+
+		for (int i = 0; i < n - 1; i++) //sort3
+		{
+			for (int j = i + 1; j < n; j++)
+			{
+				if ((FirstDig(mas[i]) == FirstDig(mas[j])) && (MinDigit(mas[i]) == MinDigit(mas[j])))
+				{
+					if (mas[i] > mas[j])
 					{
 						std::swap(mas[i], mas[j]);
 					}
 				}
 			}
 		}
-
 
 		for (int i = 0; i < n; i++)
 		{
@@ -93,7 +113,7 @@ int main()
 	}
 }
 
-/*
+/* Без Функции - доработать 
 int main()
 {
 	setlocale(LC_ALL, "Rus");
@@ -102,7 +122,7 @@ int main()
 	std::ofstream out("output.txt");
 	if (in)
 	{
-		int n, OneMasI , OneMasJ , SumI = 0, SumJ = 0; 
+		int n, OneMasI, OneMasJ, Imin, Jmin, OstatI, OstatJ; 
 		int mas[N];
 		in >> n;
 		for (int i = 0; i < n; i++)
@@ -119,42 +139,97 @@ int main()
 
 		for (int i = 0; i < n-1; i++) // первая сортировка
 		{
-			SumI = 0;
 			OneMasI = mas[i];
-			while (OneMasI > 9)
+			while ((OneMasI/10) > 0)
 			{
-				SumI += (OneMasI % 10); // Считаем сумму цифр в числе i
 				OneMasI = OneMasI / 10;
 			}
 			for (int j = i+1; j < n; j++)
+			{
+				OneMasJ = mas[j];
+				while ((OneMasJ/10) > 0)
 				{
-					OneMasJ = mas[j];
-					SumJ = 0;
-					while (OneMasJ > 9)
-					{
-						SumJ += (OneMasJ % 10);
-						OneMasJ /= 10;
-					}
-					if (OneMasI != OneMasJ)
-					{
-						if (OneMasI > OneMasJ)
-						{
-							std::swap(mas[i], mas[j]);
-						}
-					}
-					else if (SumI != SumJ)
-					{
-						if (SumI > SumJ)
-						{
-							std::swap(mas[i], mas[j]);
-						}
-					}
-					else if (mas[i] > mas[j])
+					OneMasJ = OneMasJ / 10;
+				}
+				if (OneMasI > OneMasJ) // sort1
 					{
 						std::swap(mas[i], mas[j]);
 					}
 				}
 		}
+
+		for (int i = 0; i < n - 1; i++) // вторая сортировка
+		{
+			Imin = 10;
+			OneMasI = mas[i];
+			while ((OneMasI / 10) > 0)
+			{
+				OstatI = OneMasI % 10; // Ищем цифры числа I
+				if (Imin > OstatI) // Минимальная цифра числа I
+				{
+					Imin = OstatI;
+				}
+				OneMasI = OneMasI / 10;
+			}
+			for (int j = i + 1; j < n; j++)
+			{
+				OneMasJ = mas[j];
+				Jmin = 10;
+				while ((OneMasJ / 10) > 0)
+				{
+					OstatJ = OneMasJ % 10; // Ищем цифры числа J
+					if (Jmin > OstatJ) // Минимальная цифра числа J
+					{
+						Jmin = OstatJ;
+					}
+					OneMasJ = OneMasJ / 10;
+				}
+				if (OneMasI == OneMasJ) // sort2
+				{
+					if (Imin > Jmin)
+					{
+						std::swap(mas[i], mas[j]);
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < n - 1; i++) // третья сортировка
+		{
+			Imin = 10;
+			OneMasI = mas[i];
+			while ((OneMasI / 10) > 0)
+			{
+				OstatI = OneMasI % 10; // Ищем цифры числа I
+				if (Imin > OstatI) // Минимальная цифра числа I
+				{
+					Imin = OstatI;
+				}
+				OneMasI = OneMasI / 10;
+			}
+			for (int j = i + 1; j < n; j++)
+			{
+				OneMasJ = mas[j];
+				Jmin = 10;
+				while ((OneMasJ / 10) > 0)
+				{
+					OstatJ = OneMasJ % 10; // Ищем цифры числа J
+					if (Jmin > OstatJ) // Минимальная цифра числа J
+					{
+						Jmin = OstatJ;
+					}
+					OneMasJ = OneMasJ / 10;
+				}
+				if ((OneMasI == OneMasJ) && (Imin == Jmin)) // sort3
+				{
+					if (mas[i] > mas[j])
+					{
+						std::swap(mas[i], mas[j]);
+					}
+				}
+			}
+		}
+
 		for (int i = 0; i < n; i++)
 		{
 			out << mas[i] << " ";
