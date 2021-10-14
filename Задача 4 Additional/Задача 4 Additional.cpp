b13b332703d8749ffef268602284354d56c01c9d
Vitaliy <61117565+poseidonnn0@users.noreply.github.com>
2021-10-14 15:35:22 +0200

    Add files via upload

---
+++ Задача 4 Additional/Задача 4 Additional.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+
+int SumElements(int n, char mas[6])
+{
+	std::fill(mas, mas + 6, 0);
+	int k = n;
+	int count = 0;
+	while (k != 0)
+	{
+		mas[count] = k % 10;
+		k /= 10;
+		count++;
+	}
+	if (mas[0] + mas[1] + mas[2] == mas[3] + mas[4] + mas[5])
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	char s[6];
+	int kol = 0;
+	for (int i = 0; i <= 999999; i++)
+	{
+		if (SumElements(i, s))
+		{
+			kol++;
+		}
+	}
+	std::cout << "Кол-во счастливых билетов = " << kol << std::endl;
+}
