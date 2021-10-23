852ce48230d34d879ff2f66f87e5db8d253115dc
Vitaliy <79005626141@mail.ru>
2021-10-24 00:54:47 +0200

    Лабораторная работа №8 на 28.10.21
    Залив выполненных заданий

---
+++ Homework on 28.10.21 8 lab/8.1. (4.2)/8.1. (4.2)/8.1. (4.2).cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+#include <fstream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+/*
+2.	Ввести последовательность натуральных чисел{Aj}, j = 1...n (n <= 1000).
+(1 часть)Упорядочить последовательность по неубыванию первой цифры числа,
+(2 часть)числа с одинаковыми первыми цифрами дополнительно упорядочить по неубыванию наименьшей цифры числа,
+(3 часть)числа с одинаковыми первыми цифрами и  одинаковыми наименьшими цифрами дополнительно упорядочить по неубыванию самого числа.
+*/
+
+
+int FirstDig(int n) // Поиск первой цифры в числе
+{
+	while ((n / 10) > 0)
+	{
+		n /= 10;
+	}
+	return n;
+}
+
+int MinDigit(int n) // Поиск минимальной цифры в числе
+{
+	int Min = 10;
+	int Ostat;
+	while ((n / 10) > 0)
+	{
+		Ostat = n % 10;
+		if (Min > Ostat)
+		{
+			Min = Ostat;
+		}
+		n /= 10;
+	}
+	return Min;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	cout << "Лабораторная работа(8.1) на 28.10.2021. Кондратьев Виталий, 4 вариант." << endl;
+	std::ifstream in("input.txt");
+	std::ofstream out("output.txt");
+	if (in)
+	{
+		int n;
+		// int mas[N];
+		in >> n;
+		int* mas = new int[n];
+		for (int i = 0; i < n; i++)
+		{
+			in >> mas[i];
+		}
+
+		for (int i = 0; i < n - 1; i++) // sort1
+		{
+			for (int j = i + 1; j < n; j++)
+			{
+				if (FirstDig(mas[i]) > FirstDig(mas[j]))
+				{
+					std::swap(mas[i], mas[j]);
+				}
+			}
+		}
+
+		for (int i = 0; i < n - 1; i++) //sort2
+		{
+			for (int j = i + 1; j < n; j++)
+			{
+				if (FirstDig(mas[i]) == FirstDig(mas[j]))
+				{
+					if (MinDigit(mas[i]) > MinDigit(mas[j]))
+					{
+						std::swap(mas[i], mas[j]);
+					}
+				}
+			}
+		}
+
+
+		for (int i = 0; i < n - 1; i++) //sort3
+		{
+			for (int j = i + 1; j < n; j++)
+			{
+				if ((FirstDig(mas[i]) == FirstDig(mas[j])) && (MinDigit(mas[i]) == MinDigit(mas[j])))
+				{
+					if (mas[i] > mas[j])
+					{
+						std::swap(mas[i], mas[j]);
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < n; i++)
+		{
+			out << mas[i] << " ";
+		}
+		delete[] mas;
+	}
+	else
+	{
+		cout << "File not found" << endl;
+	}
+}
---
+++ Homework on 28.10.21 8 lab/8.2 (4.3)/8.2/8.2/8.2.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <fstream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+// 3.	Дана целочисленная матрица {Aij}i=1..n,j=1..m (n,m<=100). Найти строку с наименьшей суммой элементов и заменнить все элементы этой строки этой суммой.
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	cout << "Лабораторная работа на 28.10.2021(8.2). Кондратьев Виталий, 4 вариант." << endl;
+	std::ifstream in("input.txt");
+	std::ofstream out("output.txt");
+	if (in)
+	{
+		int n, m, summin = INT_MAX, sum = 0, stringmin = 0;
+		in >> n >> m; // кол-во строк и столбцов в матрице считываем из файла
+
+		// Создаём динамический массив
+		int** matrix = new int*[n]; // задаём двумерерный массив с n-строк, подразумевая, что в каждой строчке есть ещё строки(столбцы)
+		for (int i = 0; i < n; i++)
+		{
+			matrix[i] = new int[m]; // задаём к каждой строке ещё строки(столбцы)
+		}
+
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				in >> matrix[i][j];
+			}
+		}
+
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				sum = sum + matrix[i][j];
+			}
+			if (summin > sum)
+			{
+				stringmin = i;
+				summin = sum;
+			}
+			sum = 0;
+		}
+
+		for (int i = stringmin; i <= stringmin; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				matrix[i][j] = summin;
+			}
+		}
+
+		out << "Минимальная сумма в строке = " << summin << endl;
+
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				out << matrix[i][j] << " ";
+			}
+			out << endl;
+		}
+
+		// Очищаем память от динамического массива
+		for (int i = 0; i < n; i++)
+		{
+			delete[] matrix[i]; // удаляем столбцы
+		}
+		delete[] matrix; // удаляем строки
+
+	}
+	else
+	{
+		cout << "File not found" << endl;
+	}
+}
