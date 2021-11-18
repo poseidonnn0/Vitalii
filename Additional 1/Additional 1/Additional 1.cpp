914b796b08a14e524cbfca6ef606bfd4c6c65d7a
Vitaliy <79005626141@mail.ru>
2021-11-18 16:52:44 +0200

    for 13 test

--- Additional 1/Additional 1/Additional 1.cpp
+++ Additional 1/Additional 1/Additional 1.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <algorithm>
 int main()
 {
 	int Q1, P1, Q2, P2, A, mA, min1 = INT_MAX, SUM=0, G, D=0, count1 = 0, count2 = 0;
@@ -10,11 +11,11 @@ int main()
 	while (G != 0)
 	{
 		SUM = G * P1 + D * P2;
-		min1 = (min1 > SUM) ? SUM : min1;
+		min1 = std::min(SUM,min1);
 		G--;
 		while (G * Q1 + D * Q2 < A) { D++; }
 	}
 	while (count2 * Q2 < A) { count2++; }
-	min1 = (min1 > count2 * P2) ? count2 * Q2 : min1;
+	min1 = std::min(SUM, min1);
 	std::cout << '\n' << min1;
 }
