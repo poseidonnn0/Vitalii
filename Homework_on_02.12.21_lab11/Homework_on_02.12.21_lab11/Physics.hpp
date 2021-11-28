1a9f62c8752fcdce021e0eba49db29a0a928648f
Vitaliy <79005626141@mail.ru>
2021-11-28 17:05:28 +0200

    lab 11

---
+++ Homework_on_02.12.21_lab11/Homework_on_02.12.21_lab11/Physics.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+
+namespace VK
+{
+	class Device
+	{
+		int address;
+		std::string name;
+		float energy, temperature;
+	public:
+		Device(std::string name1, int address1);
+		~Device();
+		void Poll();
+		void Print();
+	};
+}
---
+++ Homework_on_02.12.21_lab11/Homework_on_02.12.21_lab11/README.md
@@ -0,0 +1,3 @@
+<p align="center">
+    <img src="https://github.com/poseidonnn0/Vitalii/raw/Homework_on_02.12.21_lab11/Homework_on_02.12.21_lab11/img/11.jpg">
+</p>
