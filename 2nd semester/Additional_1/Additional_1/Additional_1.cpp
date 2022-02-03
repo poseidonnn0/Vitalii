cd44f6ca1aad78a46559ee35a966feec5dc3420b
Vitaliy <79005626141@mail.ru>
2022-02-03 03:08:37 +0200

    Additional_1

---
+++ 2nd semester/Additional_1/Additional_1/Additional_1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <fstream>
+
+const int countNumbers = 1000;
+
+int MaxNumber(int *mas, int start, int countNubmers) // 
+{
+	int NumberNext;
+	if (start == countNubmers-1) // 1000 - 1 → 0-999 элементы в массиве, не включая 1000
+	{
+		return start;
+	}
+	NumberNext = MaxNumber(mas, start + 1, countNubmers);
+	if (mas[NumberNext] > mas[start])
+	{
+		return NumberNext;
+	}
+	else return start;
+}
+
+int main()
+{
+	std::ifstream in("output.txt");
+	int mas[countNumbers];
+	for (int i = 0; i < countNumbers; i++)
+	{
+		in >> mas[i];
+	}
+	int max =  MaxNumber(mas, 0, countNumbers);
+	std::cout << "INDEX max = " << max << "\nmas[max] = " << mas[max] << '\n';
+}
+
