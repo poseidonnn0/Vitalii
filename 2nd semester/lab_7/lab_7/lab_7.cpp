b68e2d0a588c76b398b18fdda21c77aa7c539e3e
Vitaliy <79005626141@mail.ru>
2022-03-24 12:23:12 +0200

    + lab_7

---
+++ 2nd semester/lab_7/lab_7/lab_7.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include "Matrix.hpp"
+
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	const int n_2 = 2;
+	const int m_2 = 2;
+	const int n_3 = 3;
+	const int m_3 = 3;
+	math::Matrix A(n_2, m_2);
+
+	std::cin >> A;
+
+	std::cout << A << '\n';
+
+	std::cout << A.Det() << '\n';
+
+	std::cout << A.transposition() << '\n';
+
+	std::cout << A.inv() << '\n';
+
+	math::Matrix B(n_3, m_3);
+
+	std::cin >> B;
+
+	std::cout << B << '\n';
+
+	std::cout << B.Det() << '\n';
+
+	std::cout << B.transposition() << '\n';
+
+	std::cout << B.inv() << '\n';
+
+	return 0;
+}
