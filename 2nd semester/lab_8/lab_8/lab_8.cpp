4a068c1f15c0277ffc6e24ff5c49f65f701b0faf
Vitaliy <79005626141@mail.ru>
2022-03-30 23:30:34 +0200

    + lab_8
    Есть вопросы к методу через шаблон, а так же к тому, как перенести шаблонные определения методов в cpp файл

---
+++ 2nd semester/lab_8/lab_8/lab_8.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+#include "Matrix.hpp"
+#include <cassert>
+
+int main()
+{
+	std::cout << "=== Test 1 ===" << std::endl;
+
+	{
+		math::Mat22d A({ {
+			 {1,2},
+			 {3,4}
+		} });
+
+		math::Vec2d X({ {
+			{1},
+			{1}
+		} });
+
+		auto B = A * X;
+
+		assert(B.get(0, 0) == 3);
+		assert(B.get(1, 0) == 7);
+	}
+
+	std::cout << "Done!" << std::endl;
+
+	std::cout << "=== Test 2 ===" << std::endl;
+
+	{
+		math::Mat22d A({ {
+			 {1,1},
+			 {1,1}
+		} });
+
+		math::Vec2d X({ {
+			{1},
+			{1}
+		} });
+
+		auto B = A * X;
+
+		assert(B.get(0, 0) == 2);
+		assert(B.get(1, 0) == 2);
+	}
+
+	std::cout << "Done!" << std::endl;
+
+	std::cout << "=== Test 3 / Determinant ===" << std::endl;
+
+	{
+		math::Mat22d A({ {
+			 {1,2},
+			 {3,10.4}
+		} });
+
+		assert(A.Det() == 4.4);
+	}
+	std::cout << "Done!" << std::endl;
+
+	std::cout << "=== Test 4 / Transposition  ===" << std::endl;
+
+	{
+		math::Mat33d A({ {
+			 {1,2,3},
+			 {4,5,6},
+			 {7,8,9}
+		}});
+
+		auto B = A.transposition();
+
+		assert(B.get(0, 1) == 4);
+		assert(B.get(1, 0) == 2);
+		assert(B.get(1, 2) == 8);
+		assert(B.get(2, 1) == 6);
+
+	}
+
+	std::cout << "Done!" << std::endl;
+
+	std::cout << "=== Test 5 / Inverse  ===" << std::endl;
+	
+	{
+		math::Mat22d A({ {
+			 {1,2},
+			 {3,5}
+		} });
+		auto B = A.inv();
+
+		assert(B.get(0, 0) == -5);
+		assert(B.get(1, 0) == 3);
+	}
+
+	std::cout << "Done!" << std::endl;
+
+
+	return 0;
+}
