f03938d567ef6ca91b232f0707db8fd44ca0dedf
Vitaliy <79005626141@mail.ru>
2022-09-10 20:40:55 +0200

    +17 lab

---
+++ 3nd semester/17_lab/17_lab/BinaryTree.hpp
@@ -0,0 +1,25 @@
+﻿
+#ifndef __BINARY_TREE_H__
+#define __BINARY_TREE_H__
+
+#include <iostream>
+
+namespace Tree
+{
+	struct Elem
+	{
+		int data;       
+		Elem* left;
+		Elem* right;
+		Elem* parent;
+	};
+	Elem* MAKE(int data, Elem* p);
+	void ADD(int data, Elem*& root);
+	void PASS(Elem* v);
+	Elem* SEARCH(int data, Elem* v);
+	void SearchCount(int data, Elem* v, int count);
+	void DELETE(int data, Elem*& root);
+	void CLEAR(Elem*& v);
+}
+
+#endif //  __BINARY_TREE_H__
---
+++ 3nd semester/17_lab/17_lab/Options.hpp
@@ -0,0 +1,11 @@
+﻿
+#ifndef __OPTIONS_H__
+#define __OPTIONS_H__
+#include "BinaryTree.hpp"
+#include <fstream>
+namespace Functions
+{
+	void Task5(char s, std::ifstream& in, Tree::Elem* &root);
+}
+
+#endif //  __OPTIONS_H__
