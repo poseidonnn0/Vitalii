a5177aebb111caeb7c950e78842d8116e829b0c9
Vitaliy <79005626141@mail.ru>
2022-12-13 03:17:14 +0200

    +15 lab

---
+++ 3nd semester/15_lab/15_lab/15_lab.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <BinaryTree.hpp>
+
+int main()
+{
+		Tree::Elem* root = nullptr;
+        int N = 20;
+        srand(time(0));
+        for (int i = 0; i < N; i++)
+        {
+            int a = i * rand() % 100;
+            Tree::ADD(a, root);
+        }
+		Tree::PASS(root);
+
+        std::cout << "\nCenter: "; // центральный обход
+        Tree::center(root);
+        std::cout << "\nByPass: "; // прямой обход
+        Tree::bypass(root);
+        std::cout << "\nEndByPass: "; // концевой обход
+        Tree::EndBypass(root);
+        std::cout << "\n\n\n";
+        Tree::CLEAR(root);
+		return 0;
+}
