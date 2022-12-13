df58ef518d551d00e0bdea31905fbb9321a629b9
Vitaliy <79005626141@mail.ru>
2022-12-13 03:16:59 +0200

    +13 lab

---
+++ 3nd semester/13_lab/13_lab/13_lab/13_lab.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <HashFunc.h>
+#include <cstdlib>
+int main()
+{
+	int hashtable[N][N]; // const int N = 18;
+	for (int i = 0; i < N; i++) 
+	{
+		for (int j = 0; j < N; j++) 
+		{
+			hashtable[i][j] = 0;
+		}
+	}
+	int mas[n]; // const int n = 15;
+
+	srand(time(0));
+
+	for (int i = 0; i < n; i++) 
+	{
+		mas[i] = i * rand() % 50;
+	}
+
+	for (int i = 0; i < n; i++) 
+	{
+		hash::HashTable(hashtable, mas[i]);
+	}
+
+	hash::out(hashtable);
+
+	//hash::foundN(1);
+	return 0;
+}
---
+++ 3nd semester/13_lab/13_lab/13_lab/HashFunc.cpp
@@ -0,0 +1,71 @@
+﻿/*
+* HashFunc.cpp
+*
+* @created 12.12.2022 Vitaliy Kondratev
+*
+*/
+#include "HashFunc.h"
+namespace hash
+{
+	int Hashfunc(int a) 
+	{
+		int b = a % N;
+		return b;
+	}
+
+	void HashTable(int mas[N][N], int a) 
+	{
+		int b = Hashfunc(a);
+		if (mas[b][0] == 0) 
+		{
+			mas[b][0] = a;
+		}
+		else 
+		{
+			for (int i = 1; i < N; i++) 
+			{
+				if (mas[b][i] == 0) 
+				{
+					mas[b][i] = a;
+					break;
+				}
+			}
+		}
+	}
+
+	void out(int mas[N][N]) 
+	{
+		for (int i = 0; i < N; i++) 
+		{
+			for (int j = 0; j < N; j++) 
+			{
+				if (mas[i][j] != 0) 
+				{
+					std::cout << mas[i][j] << " ";
+				}
+			}
+			std::cout << std::endl;
+		}
+	}
+
+	int foundN(int y) 
+	{
+		int a = Hashfunc(y);
+		return a;
+	}
+
+	void foundNubmers(int mas[N][N], int k) 
+	{
+		for (int i = 0; i < N; i++) 
+		{
+			if (mas[k][i] != 0) 
+			{
+				std::cout << "foundNumbers: " << mas[k][i] << " ";
+			}
+			else 
+			{
+				break;
+			}
+		}
+	}
+}
