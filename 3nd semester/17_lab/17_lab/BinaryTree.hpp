83751035d9a9ba5d24c32003835922cc00025dd3
Vitaliy <79005626141@mail.ru>
2022-12-13 16:09:06 +0200

    + comments

--- 3nd semester/17_lab/17_lab/BinaryTree.hpp
+++ 3nd semester/17_lab/17_lab/BinaryTree.hpp
@@ -3,23 +3,37 @@
 #define __BINARY_TREE_H__
 
 #include <iostream>
+#include <string>
 
 namespace Tree
 {
-	struct Elem
-	{
-		int data;       
-		Elem* left;
-		Elem* right;
-		Elem* parent;
-	};
-	Elem* MAKE(int data, Elem* p);
-	void ADD(int data, Elem*& root);
-	void PASS(Elem* v);
-	Elem* SEARCH(int data, Elem* v);
-	void SearchCount(int data, Elem* v, int count);
-	void DELETE(int data, Elem*& root);
-	void CLEAR(Elem*& v);
+    class Tree
+    {
+    public:
+        int value;
+        Tree* parent;
+        Tree* left;
+        Tree* right;
+        std::string string; // для линейно-скобочной записи.
+
+        Tree(const int v, Tree* p); //конструктор класса дерева
+        Tree(); //переопределение конструктора
+        Tree(std::string str); // для переприсовения строки
+        void add(const int v); // добавить элемент в дерево
+
+        Tree* search(Tree* item, int v); // поиск поддерева по элементу 
+ 
+        Tree* Search(int v);
+        void remove(int value);
+        std::string toString(int value); // переводит число в строку
+    
+        void print(Tree* tree); // переводит дерево в линейно-скобочуню запись
+        void Print(); // вывести указатель на print
+        void show(); // вывод всеъ элементов
+        bool isDigital(const char elem); // проверка на число
+        Tree parse(std::string str); // парсит  строку в бинарное дерево
+    };
+
 }
 
 #endif //  __BINARY_TREE_H__
